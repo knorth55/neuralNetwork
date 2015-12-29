46c4e699e73b4f897bb7d6e75f679bc1a03494f0
Shingo Kitagawa <shingogo@hotmail.co.jp>
2015-12-30 01:13:20 +0900

    change into class

--- src/main/scala/neuralNetwork.scala
+++ src/main/scala/neuralNetwork.scala
@@ -6,16 +6,14 @@ import math.{E,pow}
 import org.opencv.core.Mat
 import org.opencv.highgui.Highgui
 
-object neuralNetwork {
-  def main(args:Array[String]): Unit ={
-    var middle: Int = 10;
-    // num of middle layer
-    var w1: collection.mutable.ArrayBuffer[collection.mutable.ArrayBuffer[Double]] = collection.mutable.ArrayBuffer.fill(middle, 100+1)(1.0);
-    //input layer -> middle layer
-    var w2: collection.mutable.ArrayBuffer[collection.mutable.ArrayBuffer[Double]] = collection.mutable.ArrayBuffer.fill(5, middle+1)(1.0);
-    //middle layer -> output layer
-    var beta: Double = 1.0;
-  }
+class neuralNetwork {
+  def middle: Int = 10;
+  // num of middle layer
+  def w1: collection.mutable.ArrayBuffer[collection.mutable.ArrayBuffer[Double]] = collection.mutable.ArrayBuffer.fill(middle, 100+1)(1.0);
+  //input layer -> middle layer
+  def w2: collection.mutable.ArrayBuffer[collection.mutable.ArrayBuffer[Double]] = collection.mutable.ArrayBuffer.fill(5, middle+1)(1.0);
+  //middle layer -> output layer
+  def beta: Double = 1.0;
 
   def backPropagation(w1: collection.mutable.ArrayBuffer[collection.mutable.ArrayBuffer[Double]], w2:collection.mutable.ArrayBuffer[collection.mutable.ArrayBuffer[Double]]):
       collection.mutable.ListBuffer[collection.mutable.ArrayBuffer[collection.mutable.ArrayBuffer[Double]]]= {
@@ -28,5 +26,3 @@ object neuralNetwork {
     return 1.0/(1.0+ pow(math.E,-sumUp*beta));
   }
 }
-
-
